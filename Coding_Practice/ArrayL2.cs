1e51f63131f9a3e9d69fedd98b2f26b3e29b5bd8
rahul08goswami <rahul008goswami@gmail.com>
2025-01-03 19:34:27 +0530

    ArrayL3

--- Coding_Practice/ArrayL2.cs
+++ Coding_Practice/ArrayL2.cs
@@ -8,6 +8,10 @@ namespace Coding_Practice
 {
     internal class ArrayL2
     {
+        private ArrayL2()
+        {
+            
+        }
         public static int[] RotateLeft(int[] arr)
         {
             //int[] arr = { 1, 2, 3, 4, 5 };
@@ -21,7 +25,7 @@ namespace Coding_Practice
                 }
                 arr[i] = arr[i + 1];
             }
-          
+
             return arr;
         }
 
@@ -44,14 +48,14 @@ namespace Coding_Practice
 
             for (int i = arr.Length - 1; i >= 0; i--)
             {
-                if(i==0)
+                if (i == 0)
                 {
                     arr[0] = temp;
                     break;
                 }
                 arr[i] = arr[i - 1];
             }
-           
+
             //for (int i = 0; i < arr.Length; i++)
             //{
             //    if (i == arr.Length - 1)
@@ -67,7 +71,7 @@ namespace Coding_Practice
             //}
             return arr;
         }
-        
+
         public static void RotateRightByNTimes(int[] arr, int t)
         {
             t = t % arr.Length;
@@ -129,6 +133,114 @@ namespace Coding_Practice
             {
                 Console.WriteLine(item);
             }
+        }
+
+        public static void LinearSearch(int num)
+        {
+            int[] arr = { 1, 2, 0, 0, 2, 3 };
+            int i = -1;
+            for (i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    Console.WriteLine($"Index of {num} is {i}");
+                    break;
+                }
+            }
+            if (i == -1)
+                Console.WriteLine(num + " Not found");
+        }
+
+        public static void Union()
+        {
+            int[] a = { 1, 2, 3, 4, 6, 2 };
+            int[] b = { 2, 3, 5 };
+
+            int j = 0;
+            //var union = a.Concat(b).Distinct().ToArray();
+            //Array.Sort(union);
+            //List<int> n = union.ToList();
+            //n.Sort();
+            var union = a.Union(b).ToArray();
+            //var res = union.ToArray();
+            Array.Sort(union);
+            foreach (var item in union)
+            {
+                Console.WriteLine(item);
+            }
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    if (a[i] == b[j])
+            //    {
+            //        j++;
+            //    }
+            //    else
+            //    {
+
+            //    }
+            //}
+        }
+        public static void Intersect()
+        {
+            int[] a = { 1, 2, 3, 4, 6, 2 };
+            int[] b = { 2, 3, 5 };
+
+            var res = a.Intersect(b);
+            //res.ToArray().Distinct();
+            foreach (var item in res)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void missingNumber(int N)
+        {
+            List<int> a = new List<int>();
+            for (int i = 0; i < N; i++)
+            {
+                a.Add(i + 1);
+            }
+            for (int i = 0; i < N; i++)
+            {
+                if (a[0] != 1)
+                {
+                    Console.WriteLine($"1 is missing");
+                    return;
+                }
+                else if (a[i] == a[i + 1] - 1)
+                {
+                    if (a[i] == a.Count - 1)
+                        return;
+                }
+                else
+                {
+                    Console.WriteLine($"{a[i + 1] - 1} is missing");
+                }
+            }
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void consecutiveOnes()
+        {
+            int[] arr = { 0, 1, 1, 1, 1, 1, 0, 1, 1 };
+            int count = 0;
+            List<int> list = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == arr.Length - 1)
+                    break;
+                if (arr[i] != arr[i + 1])
+                {
+                    count = 0;
+                }
+                else
+                {
+                    count++;
+                    list.Add(count);
+                }
+            }
+            Console.WriteLine(list.Max() + 1);
         }
     }
 }
--- Coding_Practice/Program.cs
+++ Coding_Practice/Program.cs
@@ -22,8 +22,30 @@ namespace CodingPractice
             //ArrayL2.RotateLeftByNTimes(arr, 2);
             //ArrayL2.RotateRightByNTimes(arr, 3);
             //ArrayL2.RotateRightByNTimesOptimal(arr,3);
-            ArrayL2.MoveZeros(arr);
+            //ArrayL2.MoveZeros(arr);
+            //ArrayL2.LinearSearch(3);
+            //ArrayL2.Union();
+            // ArrayL2.Intersect();
+            //ArrayL2.missingNumber(6);
+            //ArrayL2.consecutiveOnes();
+            //Fun1("rahul");
             
+
         }
+
+        public static void Fun1(string str)
+        {
+            str.ToLower();
+            //string s = str[0].ToString();
+            //s.ToUpper();
+            //s.ToArray();
+            //string st = str[0].ToString().ToUpper() + str.Substring(1);
+            string st = string.Concat(str[0].ToString().ToUpper(), str.Substring(1));
+            //var s = str.Substring(1);
+            //var n = s.ToUpper();
+
+            Console.WriteLine(st);
+
+     }
     }
 }
